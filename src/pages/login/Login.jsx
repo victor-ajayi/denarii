dadc4ea4e2da6c3d2f227fc1a6d252f30580aeab
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-06-07 14:42:08 +0300

    feat: add login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,3 +1,69 @@
+import { useState } from "react";
+import { useSignIn } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
+import "./Form.css";
+import { loginToAPI } from "./utils";
+
 export default function Login() {
-  return <h1>Login goes here</h1>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const signIn = useSignIn();
+  const navigate = useNavigate();
+
+  async function handleSubmit() {
+    const data = await loginToAPI(formData);
+
+    if (
+      signIn({
+        token: data.access_token,
+        tokenType: "Bearer",
+        authState: data.email,
+        expiresIn: data.expires_in,
+      })
+    ) {
+      navigate("/dashboard");
+    }
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+  };
+
+  return (
+    <form className="form" onSubmit={(e) => e.preventDefault()}>
+      <h1>Log in to your account</h1>
+      <label htmlFor="email">Email</label>
+      <input
+        className="form--email"
+        type="email"
+        placeholder="Email"
+        name="email"
+        required
+        onChange={handleChange}
+        value={formData.email}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="form--password"
+        type="password"
+        placeholder="Password"
+        name="password"
+        required
+        onChange={handleChange}
+        value={formData.password}
+      />
+      <button type="submit" onClick={handleSubmit}>
+        Log in
+      </button>
+      <h2 className="check-sign-in"></h2>
+    </form>
+  );
 }
