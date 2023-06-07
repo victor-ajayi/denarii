9bffe223db3ac56401d33e31e9622441df675c73
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-06-07 14:44:11 +0300

    feat: style cards with negative balance

--- src/pages/dashboard/CardItem.jsx
+++ src/pages/dashboard/CardItem.jsx
@@ -1,15 +1,23 @@
 import { UilCreditCard } from "@iconscout/react-unicons";
 
 export default function CardItem(props) {
+  const balance = props.balance.toLocaleString("ru-RU", {
+    style: "currency",
+    currency: props.currency,
+  });
+
   return (
-    <div className="cards-group--card">
+    <div className="cards-group--item">
+      {/* TODO: Render a different icon depending on ${props.type} */}
       <UilCreditCard size="32px" color="#317121" />
       <div>
-        <div className="card-balance">
-          {props.balance.toLocaleString("ru-RU", {
-            style: "currency",
-            currency: props.currency,
-          })}
+        <div
+          className="card-balance"
+          style={props.balance < 0 ? { color: "red" } : { color: "black" }}
+        >
+          {props.balance < 0
+            ? `${balance.charAt(0)} ${balance.slice(1)}`
+            : balance}
         </div>
         <span className="card-name">{props.name}</span>
       </div>
