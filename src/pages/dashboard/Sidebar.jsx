677eaccd5aba526f220cde94938d54d083e1ce20
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-06-15 16:10:17 +0300

    style: update icon colors

--- src/pages/dashboard/Sidebar.jsx
+++ src/pages/dashboard/Sidebar.jsx
@@ -11,20 +11,20 @@ export default function Sidebar() {
     <div className="sidebar">
       <div className="sidebar--top">
         <a href="#" className="sidebar-link">
-          <UilUser color="#317121" />
+          <UilUser color="#2B4EB1" />
           Profile
         </a>
         <a href="#" className="sidebar-link">
-          <UilEstate color="#317121" />
+          <UilEstate color="#2B4EB1" />
           Dashboard
         </a>
         <a href="#" className="sidebar-link">
-          <UilListUl color="#317121" />
+          <UilListUl color="#2B4EB1" />
           Categories
         </a>
       </div>
       <a href="#" className="sidebar-link">
-        <UilCog color="#317121" />
+        <UilCog color="#2B4EB1" />
         Settings
       </a>
     </div>
